135243ac86187a2aadccdfc45df0049dc7a34169
B0cz3k <lukasz.borak03@gmail.com>
2024-04-23 23:24:36 +0200

    jkhb

--- utils.py
+++ utils.py
@@ -37,8 +37,6 @@ class Encoder():
     def __init__(self, model_name, model_kwargs) -> None:
         self.embedding_function = HuggingFaceEmbeddings(
             model_name = model_name,
-            model_kwargs = model_kwargs,
-            token=TOKEN
         )
 
 def load_pdf_bytes(pdf_bytes):
